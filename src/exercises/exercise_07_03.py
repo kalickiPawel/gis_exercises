f3aed7d6189712085f555bacc8fcf56d5089b231
Paweł <kalicki.paul@icloud.com>
2021-04-26 01:10:07 +0200

    :hotfix: Changed run parameters

--- src/exercises/exercise_07_03.py
+++ src/exercises/exercise_07_03.py
@@ -6,10 +6,10 @@ def run(export_folder):
 
     box = '299000,446000,299100,446100'
     filename = 'zad03'
-    num_of_tiles = 4
+    num_of_tiles = 8
     tile = 200
 
     wms = get_data('https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution', 0, '1.3.0')
     get_tiles(wms, num_of_tiles, tile, box, export_folder, filename)
 
-    print("-->Ended exercise 01 laboratory 07<--")
+    print("-->Ended exercise 03 laboratory 07<--")
