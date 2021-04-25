d9a4ec391e3cd8f3fa770f011443abea25d0e4b8
Paweł <kalicki.paul@icloud.com>
2021-04-26 00:15:29 +0200

    :sparkles: Added exercise 03 from laboratory 07

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from src.exercises import exercise_07_01, exercise_07_02
+from src.exercises import exercise_07_01, exercise_07_02, exercise_07_03
 
 if __name__ == "__main__":
     export_folder = 'data'
     exercise_07_01.run(export_folder)
     exercise_07_02.run(export_folder)
+    exercise_07_03.run(export_folder)
