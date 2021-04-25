e680fb513a659649268ff81ea3701121173ecc85
Paweł <kalicki.paul@icloud.com>
2021-04-25 21:52:33 +0200

    :art: Moved get_layer info from wms/wfs to new function

--- src/get_from_wms_or_wfs.py
+++ src/get_from_wms_or_wfs.py
@@ -1,15 +1,17 @@
 from owslib.wms import WebMapService
 from owslib.wfs import WebFeatureService
 
+import numpy as np
+import os
 
-def get_data(url, wfs, v, layer):
+
+def get_data(url, wfs, v):
     """
     This function makes it possible to download data
     via WMS and WFS services.
     :param url: address of service
     :param wfs: 0 - wms, 1 - wfs
     :param v: service version
-    :param layer: name of layer in service
     :return: wms or wfs object
     """
     if not wfs:
@@ -18,17 +20,23 @@ def get_data(url, wfs, v, layer):
         print(f"Title: {wms.identification.title}")
         print(f"Abstract: {wms.identification.abstract}")
         print(f"List of contents: {list(wms.contents)}")
-
-        print(f"Dataset name: {wms[layer].title}")
-        print(f"CRS: {wms[layer].crsOptions}")
-        return wms, wms[layer], wms[layer].title.replace(" ", "_").lower()
+        return wms
     else:
         wfs = WebFeatureService(url, version=v) if v is not None else WebFeatureService(url)
         print(f"{wfs.identification.type} {wfs.identification.version}")
         print(f"Title: {wfs.identification.title}")
         print(f"Abstract: {wfs.identification.abstract}")
         print(f"List of contents: {list(wfs.contents)}")
+        return wfs
 
-        print(f"Dataset name: {wfs[layer].title}")
-        print(f"CRS: {wfs[layer].crsOptions}")
-        return wfs, wfs[layer], wfs[layer].title.replace(" ", "_").lower()
+
+def get_layer(service, layer):
+    """
+    Get information from WMS or WFS for specific layer
+    :param service: WMS or WFS service object
+    :param layer: name of layer in service
+    :return: service of layer, name of dataset
+    """
+    print(f"Dataset name: {service[layer].title}")
+    print(f"CRS: {service[layer].crsOptions}")
+    return service[layer], service[layer].title.replace(" ", "_").lower()
