524037e87f46b0617cec99f92c2f8b04d2ef38d6
Paweł <kalicki.paul@icloud.com>
2021-04-26 00:46:20 +0200

    :art: Moved logic to function

--- src/exercises/exercise_07_03.py
+++ src/exercises/exercise_07_03.py
@@ -1,67 +1,15 @@
-from itertools import product
-
-from osgeo import osr
-from osgeo import gdal
-
-import matplotlib.image as mpimg
-import numpy as np
-import time
-import os
-
-from src.get_from_wms_or_wfs import get_data, get_map
+from src.get_from_wms_or_wfs import get_data, get_map, get_tiles
 
 
 def run(export_folder):
-    box0, box1, box2, box3 = box = np.fromstring('299000,446000,299100,446100', dtype=float, sep=',')
+    print("-->Started exercise 03 laboratory 07<--")
 
+    box = '299000,446000,299100,446100'
+    filename = 'zad03'
+    num_of_tiles = 4
     tile = 200
-    size = (box[2] - box[0])
-    resolution = size / tile
 
     wms = get_data('https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution', 0, '1.3.0')
+    get_tiles(wms, num_of_tiles, tile, box, export_folder, filename)
 
-    for i in range(4):
-        box0, box2 = (box[0], box[2])
-        for j in range(4):
-            raster = {
-                'name': f'tile_{i}{j}',
-                'extension': 'jpeg',
-                'size': (tile,)*2,
-                'format': 'image/jpeg'
-            }
-
-            get_map(wms, 'Raster', raster, f'{box1},{box0},{box3},{box2}', export_folder)
-            img = mpimg.imread(os.path.join(export_folder, f"{raster['name']}.{raster['extension']}"))
-
-            dst_ds = gdal.GetDriverByName('GTiff').Create(
-                os.path.join(export_folder, f"{raster['name']}.tif"),
-                tile, tile, 3, gdal.GDT_Byte
-            )
-
-            dst_ds.SetGeoTransform((box1, resolution, 0, box2, 0, -resolution))
-
-            srs = osr.SpatialReference()
-            srs.ImportFromEPSG(2180)
-
-            dst_ds.SetProjection(srs.ExportToWkt())
-            for k in range(3):
-                dst_ds.GetRasterBand(k+1).WriteArray(img[:, :, k])
-            dst_ds.FlushCache()
-            dst_ds = None
-
-            box0 += size
-            box2 += size
-            time.sleep(2)
-
-        box1 += size
-        box3 += size
-
-    tif_files = [f"{export_folder}/tile_{el[0]}{el[1]}.tif" for el in product(range(4), repeat=2)]
-    vrt = gdal.BuildVRT(os.path.join(export_folder, "zad03_merged.vrt"), tif_files)
-    gdal.Translate(os.path.join(export_folder, "zad03_merged.tif"), vrt, xRes=resolution, yRes=-resolution)
-    vrt = None
-
-    os.remove(os.path.join(export_folder, "zad03_merged.vrt"))
-    for i in tif_files:
-        os.remove(i)
-        os.remove(i.replace("tif", "jpeg"))
+    print("-->Ended exercise 01 laboratory 07<--")
--- src/get_from_wms_or_wfs.py
+++ src/get_from_wms_or_wfs.py
@@ -1,7 +1,13 @@
 from owslib.wms import WebMapService
 from owslib.wfs import WebFeatureService
+from itertools import product
 
+from osgeo import osr
+from osgeo import gdal
+
+import matplotlib.image as mpimg
 import numpy as np
+import time
 import os
 
 
@@ -42,9 +48,19 @@ def get_layer(service, layer):
     return service[layer], service[layer].title.replace(" ", "_").lower()
 
 
-def get_map(wms, layer, raster, bbox, export_folder, styles='default', srs='EPSG:2180'):
-
-    resp = wms.getmap(
+def get_map(service, layer, raster, bbox, export_folder, styles='default', srs='EPSG:2180'):
+    """
+    Get map from WMS or WFS for specific bbox and resolution
+    :param service: WMS or WFS service object
+    :param layer: name of layer in service
+    :param raster: dictionary with 'name', 'size', 'format' and 'extension' of raster
+    :param bbox: bbox format in string
+    :param export_folder: name of export data folder
+    :param styles: string of styles of map
+    :param srs: specific CRS
+    :return: response from service
+    """
+    resp = service.getmap(
         layers=[layer],
         styles=[styles],
         srs=srs,
@@ -60,8 +76,19 @@ def get_map(wms, layer, raster, bbox, export_folder, styles='default', srs='EPSG
     return resp
 
 
-def get_center_tile(wms, layer, raster, bbox, export_folder, styles='default', srs='EPSG:2180'):
-    resp = wms.getfeatureinfo(
+def get_center_tile(service, layer, raster, bbox, export_folder, styles='default', srs='EPSG:2180'):
+    """
+    Get WMS or WFS information from center the tile
+    :param service: WMS or WFS service object
+    :param layer: name of layer in service
+    :param raster: dictionary with 'name', 'size', 'format' and 'extension' of raster
+    :param bbox: bbox format in string
+    :param export_folder: name of export data folder
+    :param styles: string of styles of map
+    :param srs: specific CRS
+    :return: response from service
+    """
+    resp = service.getfeatureinfo(
         layers=[layer],
         styles=[styles],
         srs=srs,
@@ -77,3 +104,68 @@ def get_center_tile(wms, layer, raster, bbox, export_folder, styles='default', s
     out.close()
 
     return resp
+
+
+def get_tiles(service, num_of_tiles, tile, bbox, export_folder, name):
+    """
+    Get GeoTiff file from WMS or WFS service,
+    for specific bbox, resolution and num of tiles
+    :param service: WMS or WFS service object
+    :param num_of_tiles: number of tiles
+    :param tile: size of one tile
+    :param bbox: bbox format in string
+    :param export_folder: name of export data folder
+    :param name: name of export tiff file
+    :return: None
+    """
+    box0, box1, box2, box3 = box = np.fromstring(bbox, dtype=float, sep=',')
+    size = (box[2] - box[0])
+    res = size / tile
+
+    for i in range(num_of_tiles):
+        box0, box2 = (box[0], box[2])
+        for j in range(num_of_tiles):
+            raster = {
+                'name': f'tile_{i}{j}',
+                'extension': 'jpeg',
+                'size': (tile,) * 2,
+                'format': 'image/jpeg'
+            }
+
+            get_map(service, 'Raster', raster, f'{box1},{box0},{box3},{box2}', export_folder)
+            img = mpimg.imread(os.path.join(export_folder, f"{raster['name']}.{raster['extension']}"))
+
+            dst_ds = gdal.GetDriverByName('GTiff').Create(
+                os.path.join(export_folder, f"{raster['name']}.tif"),
+                tile, tile, 3, gdal.GDT_Byte
+            )
+
+            dst_ds.SetGeoTransform((box1, res, 0, box2, 0, -res))
+
+            srs = osr.SpatialReference()
+            srs.ImportFromEPSG(2180)
+
+            dst_ds.SetProjection(srs.ExportToWkt())
+            for k in range(3):
+                dst_ds.GetRasterBand(k + 1).WriteArray(img[:, :, k])
+            dst_ds.FlushCache()
+            dst_ds = None
+
+            box0 += size
+            box2 += size
+            time.sleep(2)
+
+        box1 += size
+        box3 += size
+
+    tif_files = [f"{export_folder}/tile_{el[0]}{el[1]}.tif" for el in product(range(num_of_tiles), repeat=2)]
+    vrt = gdal.BuildVRT(os.path.join(export_folder, f"{name}_merged.vrt"), tif_files)
+    gdal.Translate(os.path.join(export_folder, f"{name}_merged.tif"), vrt, xRes=res, yRes=-res)
+    vrt = None
+
+    os.remove(os.path.join(export_folder, f"{name}_merged.vrt"))
+    for i in tif_files:
+        os.remove(i)
+        os.remove(i.replace("tif", "jpeg"))
+
+    return None
